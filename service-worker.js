01fb587486ff7fe0d214fd50114dc50b6cd278bd
osielmaya <osielmaya@yahoo.com>
2025-08-11 02:37:53 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service worker with auto-refresh (responds to SKIP_WAITING)
-const CACHE_NAME = 'golf9-auto-v16';
+const CACHE_NAME = 'golf9-auto-v17';
 const ASSETS = [
   './',
   './index.html',
